34e0a546957b8425c95fc426b9caba3704305783
Renato Navarro <devrenatonavarro@gmail.com>
2023-01-05 13:29:40 -0500

    Servicios y data externa

--- src/app/pages/posts/posts.component.ts
+++ src/app/pages/posts/posts.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DataService } from '../../services/data.service';
 
 @Component({
   selector: 'app-posts',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class PostsComponent {
 
+  constructor(
+    private dataService: DataService
+  ){}
+
+  ngOnInit() {
+    this.dataService.getPosts()
+    .subscribe( posts => {
+      console.log(posts)
+    })
+
+  }
 }
